a309ef3aee1e8034e9ee4244c73cbe3058a9ab9f
Francisco Vieira <franciscovieira@Francisco-Web.local>
2023-06-26 18:42:01 -0300

    style: CSS-in-JS - Loading Component

--- App.tsx
+++ App.tsx
@@ -5,6 +5,7 @@ import {
 	Roboto_700Bold,
 } from '@expo-google-fonts/roboto';
 import theme from './src/theme/index';
+import { StatusBar } from 'react-native';
 import { ThemeProvider } from 'styled-components';
 import { Loading } from '@components/Loading/index';
 
@@ -13,6 +14,11 @@ export default function App() {
 
 	return (
 		<ThemeProvider theme={theme}>
+			<StatusBar
+				barStyle="light-content"
+				backgroundColor="transparent"
+				translucent
+			/>
 			{fontsLoader ? <Groups /> : <Loading />}
 		</ThemeProvider>
 	);
