93c54de1a25cb70b199566a1fcd0e57434ff3bd4
Francisco Vieira <franciscovieira@Francisco-Web.local>
2023-06-26 18:19:26 -0300

    style: CSS-in-JS - Fontes personalizadas

--- App.tsx
+++ App.tsx
@@ -1,11 +1,19 @@
 import { Groups } from '@screens/Groups';
+import { ActivityIndicator } from 'react-native';
+import {
+	useFonts,
+	Roboto_400Regular,
+	Roboto_700Bold,
+} from '@expo-google-fonts/roboto';
 import theme from './src/theme/index';
 import { ThemeProvider } from 'styled-components';
 
 export default function App() {
+	const [fontsLoader] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
 	return (
 		<ThemeProvider theme={theme}>
-			<Groups />
+			{fontsLoader ? <Groups /> : <ActivityIndicator />}
 		</ThemeProvider>
 	);
 }
