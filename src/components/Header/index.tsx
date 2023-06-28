4190dee401e4932255d12dbcb5d9d08c589c6556
Francisco Vieira <franciscovieira@Francisco-Web.local>
2023-06-28 15:29:15 -0300

    style: CSS-in-JS - Icon Library

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,19 @@
-import { Container, Logo } from './style';
+import { Container, Logo, BackIcon, BackButton } from './style';
 
 import logoImg from '@assets/logo.png';
 
-export function Header() {
+type Props = {
+	showBackButton?: boolean;
+};
+
+export function Header({ showBackButton = false }: Props) {
 	return (
 		<Container>
+			{showBackButton && (
+				<BackButton>
+					<BackIcon />
+				</BackButton>
+			)}
 			<Logo source={logoImg} />
 		</Container>
 	);
