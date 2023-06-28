5c8aec755015776c01f7958f19be2eada2fcb61a
Francisco Vieira <franciscovieira@Francisco-Web.local>
2023-06-28 17:03:20 -0300

    feat: listing groups

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,14 +1,25 @@
+import { useState } from 'react';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { GroupCard } from '@components/GroupCard';
 import { Container } from './styles';
+import { FlatList } from 'react-native';
 
 export function Groups() {
+	const [groups, setGroups] = useState<string[]>([
+		'Galera da rocket',
+		'amigos',
+	]);
 	return (
 		<Container>
 			<Header />
 			<Highlight title="Turmas" subtitle="jogue com a sua turma" />
-			<GroupCard title="Galera do Ignite" />
+
+			<FlatList
+				data={groups}
+				keyExtractor={(item) => item}
+				renderItem={({ item }) => <GroupCard title={item} />}
+			/>
 		</Container>
 	);
 }
