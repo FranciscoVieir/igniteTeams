98c24b3fc5ec70073c713ed37c471816bedd5dad
Francisco Vieira <franciscovieira@Francisco-Web.local>
2023-07-12 17:20:08 -0300

    style: CSS-in-JS - Creating Button Component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -5,6 +5,7 @@ import { GroupCard } from '@components/GroupCard';
 import { Container } from './styles';
 import { FlatList } from 'react-native';
 import { ListEmpty } from '@components/ListEmpty';
+import { Button } from '@components/Button';
 
 export function Groups() {
 	const [groups, setGroups] = useState<string[]>([
@@ -25,6 +26,8 @@ export function Groups() {
 					<ListEmpty message="Que tal cadastrar a primeira turma?" />
 				)}
 			/>
+
+			<Button title="Criar uma nova Turma" />
 		</Container>
 	);
 }
