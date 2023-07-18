bd3c6b06d9e9fc17c44569af3aea7b28d49cb1cb
Francisco Vieira <franciscovieira@Francisco-Web.local>
2023-07-18 16:59:43 -0300

    build: Installing Stack Navigator

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -25,6 +25,7 @@ export function Groups() {
 				ListEmptyComponent={() => (
 					<ListEmpty message="Que tal cadastrar a primeira turma?" />
 				)}
+				showsVerticalScrollIndicator={false}
 			/>
 
 			<Button title="Criar uma nova Turma" />
